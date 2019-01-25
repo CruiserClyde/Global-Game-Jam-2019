944fabdf1efad8c665edf2b3910af9fca59c4fb0
ttgc <ttgc@users.noreply.github.com>
2019-01-25 18:24:24 +0100

    initial commit

---
+++ src/main/Launcher.java
@@ -0,0 +1,25 @@
+package main;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Launcher extends StateBasedGame {
+
+	public Launcher(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initStatesList(GameContainer arg0) throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

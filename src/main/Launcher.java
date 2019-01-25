e16e0fd612b614346ccc13226f607e603264a500
ttgc <ttgc@users.noreply.github.com>
2019-01-25 21:48:42 +0100

    begin of the game

--- src/main/Launcher.java
+++ src/main/Launcher.java
@@ -1,8 +1,11 @@
 package main;
 
+import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
+
+import states.TitleScreen;
 
 public class Launcher extends StateBasedGame {
 
@@ -14,12 +17,23 @@ public class Launcher extends StateBasedGame {
 	@Override
 	public void initStatesList(GameContainer gc) throws SlickException {
 		// TODO Auto-generated method stub
+		addState(new TitleScreen());
 
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		AppGameContainer app;
+		try {
+			app = new AppGameContainer(new Launcher("Z'homebies"));
+			app.setDisplayMode(800, 600, false);
+			app.setShowFPS(false);
+			app.setFullscreen(false);
+			app.setIcon("Pictures/icon.png");
+			app.start();
+		} catch(SlickException e){
+			e.printStackTrace();
+		}
 	}
 
 }

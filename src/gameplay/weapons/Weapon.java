f891f808fd4aa351bf2dea2e97f1b55ed4c0b4b1
ttgc <ttgc@users.noreply.github.com>
2019-01-26 12:22:03 +0100

    repair build path

--- src/gameplay/weapons/Weapon.java
+++ src/gameplay/weapons/Weapon.java
@@ -2,18 +2,22 @@ package gameplay.weapons;
 
 import java.io.Serializable;
 
+import gameplay.characters.Entity;
 import gameplay.interfaces.Rendering;
+import pathfinderlib.basics.Matrix;
 
 public abstract class Weapon implements Serializable, Rendering {
+	private static final long serialVersionUID = 1L;
 	private int x;
 	private int y;
 	private int hp;
 	private int charge;
+	private int range;
 
 	public Weapon() {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public abstract void trigger();
+	public abstract void trigger(Matrix<Entity> entities);
 
 }

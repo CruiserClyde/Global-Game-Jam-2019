be056608a120657f0c40da831cf2c719a6124d8e
ttgc <ttgc@users.noreply.github.com>
2019-01-27 02:01:23 +0100

    return to the past now

--- src/gameplay/characters/BigZombie.java
+++ src/gameplay/characters/BigZombie.java
@@ -6,7 +6,7 @@ public class BigZombie extends Ennemies{
 	private static final long serialVersionUID = 7851604530872540580L;
 
 	public BigZombie(StateBasedGame sbg) {
-		super((int)Math.random()*24, (int)Math.random()*24, 100, 10, 10, "zombie1", sbg);
+		super((int)Math.random()*24, (int)Math.random()*24, 100, 10, 10, "zombie", sbg);
 		// TODO Auto-generated constructor stub
 	}
 
